8c624383bdf4641ef96780e779068de5a44296b6
Jack Chen <yuxi.chen@berkeley.edu>
2016-08-03 13:08:37 -0700

    Update structsInSwift.swift

--- structsInSwift.swift
+++ structsInSwift.swift
@@ -10,20 +10,20 @@ tutorial2.difficulty = 2
 
 //What is the value of tutorial1.difficulty and tutorial2.difficulty?
 
-First let's run through the code: 
-tutorial1 is assigned to a Tutorial object of struct type. 
-tutorial2 is assigned to the value that was copied over from tutorial1.
-We update tutorial2's attribute difficulty to 2. This means that the result is:
+#First let's run through the code: 
+#tutorial1 is assigned to a Tutorial object of struct type. 
+#tutorial2 is assigned to the value that was copied over from tutorial1.
+#We update tutorial2's attribute difficulty to 2. This means that the result is:
 
-tutorial1.difficulty -> 1
-tutorial2.difficulty -> 2
+#tutorial1.difficulty -> 1
+#tutorial2.difficulty -> 2
 
-Here's the explanation: 
-Structures in Swift are value types, so they are copied by value rather than by reference. 
-In this line:      var tutorial2 = tutorial1, the value from tutorial1 is copied over and assigned to tutorial2. Thus
-any change to tutorial2 will not be reflected by tutorial1.
+#Here's the explanation: 
+#Structures in Swift are value types, so they are copied by value rather than by reference. 
+#In this line:      var tutorial2 = tutorial1, the value from tutorial1 is copied over and assigned to tutorial2. Thus
+#any change to tutorial2 will not be reflected by tutorial1.
 
 
 
 //Now, consider if Tutorial was instead a class. What would happen?
-If Tutorial was a class, things are passed by reference. Thus, changes to tutorial2 would reflect a change to tutorial1 and vice versa. 
+#If Tutorial was a class, things are passed by reference. Thus, changes to tutorial2 would reflect a change to tutorial1 and vice versa. 
