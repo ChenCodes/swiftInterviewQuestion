372fca745abefb29914af51f61570d896196dcc3
Jack Chen <yuxi.chen@berkeley.edu>
2016-08-03 13:00:34 -0700

    created idealForLoop

---
+++ idealForLoop.swift
@@ -0,0 +1,17 @@
+//Question 1:
+
+//What is the best way to write a for loop in swift?
+
+//Choice A:
+for var i = 0; i < 5; i++ {
+  print("Hello!")
+}
+
+//Choice B: - Closed Operator 
+for _ in 0...4 {
+  print("Hello!")
+}
+
+
+//Choice B is better, it performs the same actions as choice A but in less declarations of variables.
+
